53de6ea17ee63eecdae18edf8d57828cac1e438d
wxcpsd2 <nicolaeandreiciovica@yahoo.com>
2020-09-28 19:28:47 +0300

    initial commit

---
+++ src/Navbar.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let showForm;
+</script>
+
+<nav class="nav">
+  <nav class="nav-center">
+    <h1 class="nav-title">budget calculator</h1>
+    <button type="button" class="nav-btn" on:click={showForm}>
+      <i class="far fa-plus-square">add expense</i>
+    </button>
+  </nav>
+</nav>
